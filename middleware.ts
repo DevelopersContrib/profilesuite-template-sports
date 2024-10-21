eb3706dd3dff4dc372a1135eea5a6470ae14c843
Chikaysci <kjabellar@gmail.com>
2024-10-21 11:55:46 +0800

    updated middleware

--- middleware.ts
+++ middleware.ts
@@ -4,8 +4,8 @@ import type { NextRequest } from 'next/server'
 
 // This function can be marked `async` if using `await` inside
 export function middleware(req: NextRequest) {
-    const host = req.headers.get('host')
-    if (host?.slice(0, 4) !== 'www.' && !host?.includes('localhost')) {
-        return NextResponse.redirect(`https://www.${host}${req.nextUrl.pathname}`, 301);
-    }
+    // const host = req.headers.get('host')
+    // if (!host?.includes('localhost')) {
+    //     return NextResponse.redirect(`https://${host}${req.nextUrl.pathname}`, 301);
+    // }
 }
